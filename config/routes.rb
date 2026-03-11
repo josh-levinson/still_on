96a6d426f9a8fe4252ecf2d01a1e965f2e45bc50
Joshua Levinson <jlevinson@linksquares.com>
2026-03-11 15:33:54 -0400

    twilio verification

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,8 @@ Rails.application.routes.draw do
   get  "onboarding/invite",   to: "onboarding#invite",         as: :onboarding_invite
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get "sms", to: "pages#sms", as: :sms_info
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
