797fe6706dcb6364579dd0a0e93ba90c1246d42e
Josh Levinson <joshua.levinson@gmail.com>
2026-04-12 17:06:30 -0400

    idk man

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module StillOn
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-    config.active_record.schema_format = :sql
+    config.active_record.schema_format = :ruby
 
     # Configuration for the application, engines, and railties goes here.
     #
